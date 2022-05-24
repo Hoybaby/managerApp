13a1ba7a24b1186bb1c9d8259dcff6f562b1906d
Hoybaby <mbartek436@gmail.com>
2022-05-24 10:55:51 -0400

    wanted to return an optional instead so i changed that in the repo. With the use of optionals i created an exception that i made to handle if it wasnt there.

--- src/main/java/com/infy/managerApp/repo/EmployeeRepo.java
+++ src/main/java/com/infy/managerApp/repo/EmployeeRepo.java
@@ -3,10 +3,12 @@ package com.infy.managerApp.repo;
 import com.infy.managerApp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
     //Spring will create a query to delete this.
     void deleteEmployeeById(Long id);
 
-    Employee findEmployeeById(Long id);
+    Optional<Employee> findEmployeeById(Long id);
 }
