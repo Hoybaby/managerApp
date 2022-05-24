4a640225bd3f3dcc1fea6aac3f8b843cce78fef5
Hoybaby <mbartek436@gmail.com>
2022-05-24 10:50:10 -0400

    adding two more methos and taking advantage of naming query to handle some of the functions i want in the service layer.

--- src/main/java/com/infy/managerApp/repo/EmployeeRepo.java
+++ src/main/java/com/infy/managerApp/repo/EmployeeRepo.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
+    //Spring will create a query to delete this.
+    void deleteEmployeeById(Long id);
+
+    Employee findEmployeeById(Long id);
 }
