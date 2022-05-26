f5d717eaf997b36804d28c2e545dd24b88eeadb2
Hoybaby <mbartek436@gmail.com>
2022-05-25 21:18:11 -0400

    adding more to my api file to handle POST and PUT methods to my database. I will be testing this with postman later. THe put methods and the POST are very similar and using the employeeService to call the methods of these additions.

---
+++ src/main/java/com/infy/managerApp/EmployeeResource.java
@@ -0,0 +1,47 @@
+package com.infy.managerApp;
+
+import com.infy.managerApp.model.Employee;
+import com.infy.managerApp.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+    private final EmployeeService employeeService;
+
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+    //this code will help find all employees
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK );
+    }
+
+    //this code will help find a singular employee
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id){
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK );
+
+    }
+
+    //need to add employee
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED );
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        Employee updateEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK );
+    }
+}
---
+++ src/main/java/com/infy/managerApp/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.infy.managerApp.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
